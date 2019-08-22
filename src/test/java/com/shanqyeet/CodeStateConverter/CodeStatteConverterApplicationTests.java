07a8f742b9a9b874adef4a2c680e01d8c4556c65
Chia Shanq Yeet <shanqyeet@gmail.com>
2019-08-22 11:58:33 +0800

    first commit to create docker image

---
+++ src/test/java/com/shanqyeet/CodeStateConverter/CodeStatteConverterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shanqyeet.CodeStateConverter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodeStatteConverterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
